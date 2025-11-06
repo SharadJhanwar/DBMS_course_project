4e728b945d14592ce621ca4ae7acdecc50b07f27
SharadJhanwar <sharadjhawar12345@gmail.com>
2025-11-06 18:39:11 +0530

    added profile and order routes to server

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import itemRoutes from "./routes/itemRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import cartRoutes from './routes/cartRoutes.js';
-
+import profileRoutes from './routes/profileRoutes.js';
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -17,6 +18,8 @@ app.use("/api/items", itemRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use('/api/cart', cartRoutes);
+app.use('/api/profile', profileRoutes);
+app.use("/api/orders", orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
